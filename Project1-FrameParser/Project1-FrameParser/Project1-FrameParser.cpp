a470f6487f1277e9f66251d8941702dcbf568554
YunYouJun <me@yunyoujun.cn>
2019-09-18 21:09:05 +0800

    init

---
+++ Project1-FrameParser/Project1-FrameParser/Project1-FrameParser.cpp
@@ -0,0 +1,22 @@
+﻿#include <fstream>
+#include <iostream>
+using namespace std;
+
+void ReadTestFrame()
+{
+	char data[100];
+	ifstream infile;
+	infile.open("test.txt");
+	infile >> data;
+	cout << data <<endl;
+	infile >> data;
+	cout << data << endl;
+
+	infile.close();
+}
+
+int main()
+{
+	ReadTestFrame();
+    std::cout << "Hello World!\n";
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Advanced-Windows-Network-Programming
+Lab Project about Advanced-Windows-Network-Programming
+
+More Info in <https://yuque.com/yunyoujun/cuc-course/advanced-windows-network-programming>.
+
